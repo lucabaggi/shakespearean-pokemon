68a3a210e035161c02e854e7e0649a1d61d29124
Luca Baggi <baggi.luca@gmail.com>
2020-09-23 00:33:30 +0200

    enabled swagger ui

--- src/main/java/it/lucabaggi/shakespeareanpokemon/controller/PokemonController.java
+++ src/main/java/it/lucabaggi/shakespeareanpokemon/controller/PokemonController.java
@@ -1,10 +1,16 @@
 package it.lucabaggi.shakespeareanpokemon.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import it.lucabaggi.shakespeareanpokemon.model.ErrorResponse;
 import it.lucabaggi.shakespeareanpokemon.model.Pokemon;
 import it.lucabaggi.shakespeareanpokemon.service.PokemonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +32,16 @@ public class PokemonController {
         this.pokemonService = pokemonService;
     }
 
-    @GetMapping("/{name}")
-    public ResponseEntity<Pokemon> getShakespeareanPokemonInfo(@PathVariable("name") String name) {
+    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Return Pokemon info with shakespearean description")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = Pokemon.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
+            @ApiResponse(code = 502, message = "Bad Gateway", response = ErrorResponse.class)
+    })
+    public ResponseEntity<Pokemon> getShakespeareanPokemonInfo(
+            @ApiParam(name = "name", value = "the Pokemon name") @PathVariable("name") String name) {
+
         log.info("Getting Shakespearean Pokemon info", kv("pokemon_name", name));
         Pokemon pokemon = pokemonService.getShakespeareanPokemon(name);
         return ResponseEntity.ok(pokemon);
